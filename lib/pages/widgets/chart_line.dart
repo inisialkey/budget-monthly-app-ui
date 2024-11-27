e3b3537849ce8f5b4fdfced47d4d00833c94eded
inisialkey <okidorvel404@gmail.com>
2024-11-27 15:18:07 +0700

    add balance page

--- lib/pages/widgets/chart_line.dart
+++ lib/pages/widgets/chart_line.dart
@@ -1,1 +1,52 @@
+import 'package:budget_monthly_ui/theme/colors.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
 
+List<Color> gradientColors = [
+  primary,
+  secondary,
+];
+LineChartData activityData() {
+  return LineChartData(
+    gridData: const FlGridData(
+      show: false,
+      drawVerticalLine: true,
+    ),
+    titlesData: const FlTitlesData(
+      show: false,
+    ),
+    borderData: FlBorderData(
+      show: false,
+    ),
+    minX: 0,
+    maxX: 11,
+    minY: 0,
+    maxY: 6,
+    lineBarsData: [
+      LineChartBarData(
+        spots: [
+          const FlSpot(0, 3),
+          const FlSpot(2.6, 2.5),
+          const FlSpot(4.9, 4),
+          const FlSpot(6.7, 3.5),
+          const FlSpot(8, 4.5),
+          const FlSpot(9.5, 3),
+          const FlSpot(11, 5),
+        ],
+        isCurved: true,
+        color: primary,
+        // colors: gradientColors,
+        barWidth: 2,
+        isStrokeCapRound: true,
+        dotData: const FlDotData(
+          show: false,
+        ),
+        belowBarData: BarAreaData(
+            show: true, gradient: LinearGradient(colors: gradientColors)
+            // colors:
+            //     gradientColors.map((color) => color.withOpacity(0.3)).toList(),
+            ),
+      ),
+    ],
+  );
+}
